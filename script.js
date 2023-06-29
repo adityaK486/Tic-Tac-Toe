676ed66c8b202e3061d2106913177edf041041db
Aditya Kumar <aditya201292@gmail.com>
2023-06-29 11:52:48 +0530

    Add function to mark on spot

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-
-const board = document.querySelector(".board");
-
 const gameBoard = (()=>{
 
-    let board   =   ["O","X","X","X","X","O","O","O","X"];
+    let board = new Array(9);
     return {board};
 })();
 
 const displayController = (()=>{
 
+    const board = document.querySelector(".board");
+    let mark = "X";
+
+    function render(arr){
+        board.textContent = "";
+        for(let i=0;i<9;i++){
+            const block = document.createElement("div"); 
+            block.classList.add("block");
+            block.setAttribute("onclick",`displayController.marker(${i},gameBoard.board)`);
+            if(arr[i]!=undefined){
+                block.textContent = arr[i];
+            }
+            board.appendChild(block);
+        }
+    }
+
+    function marker(i,board){
+        if(board[i]==undefined){
+            board[i] = mark;
+            render(board);
+        }
+        mark = (mark=="X")?"O":"X";
+    }
+
+    return {render,marker};
 })();
 
 const Player = (name,symbol) => {
@@ -20,15 +42,6 @@ const Player = (name,symbol) => {
 const player1 = Player("Tanjiro");
 const player2 = Player("Muzan");
 
-function render(arr){
-    for(let i=0;i<9;i++){
-        const block = document.createElement("div"); 
-        block.classList.add("block");
-        block.textContent = arr[i];
-        board.appendChild(block);
-    }
-}
 
-render(gameBoard.board);
+displayController.render(gameBoard.board);
 
-// COMMIT 
