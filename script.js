66bb701c22352170b58d2c0210a21c81f9f90ed1
Aditya Kumar <aditya201292@gmail.com>
2023-07-01 00:06:47 +0530

    Allow to add names,includes restart btn and display winner

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const gameBoard = (()=>{
     let board = new Array(9);
     const getBoard =() => board;
 
-    let winner ;
+    let winner;
+
+    const secDialog = document.getElementById("secDialog");
+    const announce = document.querySelector(".result");
 
     function check (a,b,c) {
         let arr = getBoard();
@@ -13,13 +16,17 @@ const gameBoard = (()=>{
         }
     }
 
-    function result(){
+    function result(p1,p2){
         let arr = getBoard();
         if(check(0,1,2)||check(3,4,5)||check(6,7,8)||check(0,3,6)||check(1,4,7)||check(2,5,8)||check(0,4,8)||check(2,4,6)){
-            return console.log(`You Won ${winner}`);
+            console.log(p1.name);
+            winner = (winner=="X")?p1.name:p2.name;
+            announce.textContent = `Winner is: ${winner}`;
+            secDialog.showModal();
         }
         if(!arr.includes(undefined)){
-            return console.log("Tie");
+            announce.textContent = "Tie";
+            secDialog.showModal();
         }
     }
 
@@ -30,6 +37,16 @@ const displayController = (()=>{
 
     const board = document.querySelector(".board");
     let mark = "X";
+
+    const player = document.querySelector(".player");
+
+    const dialog  = document.getElementById("favDialog");
+    const player1  = document.getElementById("player1");
+    const player2  = document.getElementById("player2");
+    const submit  = document.getElementById("confirmBtn");
+
+    let p1;
+    let p2;
 
     function render(arr){
         board.textContent = "";
@@ -45,15 +62,32 @@ const displayController = (()=>{
     }
 
     function marker(i,board){
+        if(board[i]!=undefined){
+            return;
+        }
         if(board[i]==undefined){
             board[i] = mark;
             render(board);
-            gameBoard.result();
+            gameBoard.result(p1,p2);
         }
         mark = (mark=="X")?"O":"X";
+        player.textContent = (player.textContent==p1.name) ? p2.name : p1.name;
     }
 
-    return {render,marker};
+    window.addEventListener("load",()=>{
+        dialog.showModal();
+    });
+
+    submit.addEventListener("click",(event)=>{
+        event.preventDefault();
+        p1 =  Player(player1.value,"X");
+        p2 = Player(player2.value,"O");
+        player.textContent = p1.name;
+        gameBoard.result(p1,p2);
+        dialog.close();
+    });
+    
+    return {render,marker,player1,player2};
 })();
 
 const Player = (name,symbol) => {
@@ -62,9 +96,4 @@ const Player = (name,symbol) => {
     return {name,symbol};
 };
 
-const player1 = Player("Tanjiro");
-const player2 = Player("Muzan");
-
-
 displayController.render(gameBoard.getBoard());
-
