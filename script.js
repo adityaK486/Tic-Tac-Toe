8655786898c6e2e3f49654197e6aa89d99934738
Aditya Kumar <aditya201292@gmail.com>
2023-06-30 16:59:46 +0530

    Add logic for winning and tie

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const gameBoard = (()=>{
 
     let board = new Array(9);
-    return {board};
+    const getBoard =() => board;
+
+    let winner ;
+
+    function check (a,b,c) {
+        let arr = getBoard();
+        if((arr[a]==arr[b] && arr[b]==arr[c])&&(arr[a]!=undefined && arr[b]!=undefined && arr[c]!=undefined)){
+            winner = arr[a];
+            return true;
+        }
+    }
+
+    function result(){
+        let arr = getBoard();
+        if(check(0,1,2)||check(3,4,5)||check(6,7,8)||check(0,3,6)||check(1,4,7)||check(2,5,8)||check(0,4,8)||check(2,4,6)){
+            return console.log(`You Won ${winner}`);
+        }
+        if(!arr.includes(undefined)){
+            return console.log("Tie");
+        }
+    }
+
+    return {getBoard,result};
 })();
 
 const displayController = (()=>{
@@ -14,7 +36,7 @@ const displayController = (()=>{
         for(let i=0;i<9;i++){
             const block = document.createElement("div"); 
             block.classList.add("block");
-            block.setAttribute("onclick",`displayController.marker(${i},gameBoard.board)`);
+            block.setAttribute("onclick",`displayController.marker(${i},gameBoard.getBoard())`);
             if(arr[i]!=undefined){
                 block.textContent = arr[i];
             }
@@ -26,6 +48,7 @@ const displayController = (()=>{
         if(board[i]==undefined){
             board[i] = mark;
             render(board);
+            gameBoard.result();
         }
         mark = (mark=="X")?"O":"X";
     }
@@ -43,5 +66,5 @@ const player1 = Player("Tanjiro");
 const player2 = Player("Muzan");
 
 
-displayController.render(gameBoard.board);
+displayController.render(gameBoard.getBoard());
 
